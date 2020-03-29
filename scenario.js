00dcf91089efd3a0ac8547a4bbe1cd70a44b7692
Onixarts <pelzak1@o2.pl>
2020-03-29 12:32:01 +0200

    custom repeat count (#2)

--- scenario.js
+++ scenario.js
@@ -6,6 +6,9 @@ module.exports = function(RED) {
       //  var flow = this.context().flow;
         var timeout = null;
         var scenarios = config.scenes;
+        var repeatType = config.repeatType || 'infinite';
+        var repeatTimes = Number(config.repeatTimes) || 1;
+        var repeatCounter = 0;
         var i = 0;
         var enabled = false;
 
@@ -14,13 +17,41 @@ module.exports = function(RED) {
         function sendScene(){
           if(enabled){
             node.send({payload:scenarios[i].value});
-            node.status({fill:"green",shape:"ring",text:"Scene "+ i + " started"});
+            var repeatString = '';
+            switch(repeatType)
+            {
+              case 'once': repeatString = '(once)'; break;
+              case 'custom':repeatString = `(${repeatCounter+1}/${repeatTimes})`; break;
+              case 'infinite': repeatString = '(inf)';break;
+            } 
+            
+            node.status({fill:"green",shape:"ring",text: `Scene ${i} started ${repeatString}`});
             timeout = setTimeout(sendScene,scenarios[i].time);
           }
           i++;
-          if(i == scenarios.length) i = 0;
+          if(i == scenarios.length) {
+            i = 0;
+            repeatCounter++;
+          }
+
+          switch(repeatType)
+          {
+            case 'once': 
+              if(repeatCounter >= 1) stop(); break;
+            case 'custom':
+              if(repeatCounter >= repeatTimes) stop(); break;
+            case 'infinite': break;
+          }          
         }
 
+        function stop(){
+          if(timeout !== null) 
+            clearTimeout(timeout);
+          enabled = false;
+          i = 0;
+          repeatCounter = 0;
+          node.status({fill:"yellow",shape:"ring",text:"Stopped"});
+        }
 
         node.on('input', function(msg) {
 
@@ -28,18 +59,14 @@ module.exports = function(RED) {
             case "start":
               enabled = true;
               i = 0;
+              repeatCounter = 0;
               if(timeout !== null) clearTimeout(timeout);
               if(!scenarios || scenarios.length == 0) node.error("No scene found. Add scenes in node configuration.");
               else
                 sendScene();
             break;
 
-            case "stop":
-              if(timeout !== null) clearTimeout(timeout);
-              enabled = true;
-              i = 0;
-              node.status({fill:"yellow",shape:"ring",text:"Stopped"});
-            break;
+            case "stop": stop(); break;
 
             case "init":
               if(msg.scenes && msg.scenes.length > 0){
